8802a0e180522d152524a3020e8f5789bbd1b425
Frajdi Malaj <f.malaj.ext@teamsystem.com>
2023-01-19 16:07:30 +0100

    Add flush sync react 17

--- React_17/src/App.jsx
+++ React_17/src/App.jsx
@@ -1,10 +1,14 @@
 import AutomaticBatchingContainer from './AutomaticBatching/AutomaticBatchingContainer'
+import FlushSync from './FlushSync/FlushSync'
 
 const App = () => <>
     <h1>React 17.0.2</h1>
     <hr/>
-    <h3>No Batching :(</h3>
+    <h3>Batching</h3>
     <AutomaticBatchingContainer />
+    <hr/>
+    <h3>FlushSync(no flush sync in react 17.0.2)</h3>
+    <FlushSync />
     <hr/>
 </>
 
---
+++ React_17/src/FlushSync/FlushSync.jsx
@@ -0,0 +1,39 @@
+import { useState, useRef, useEffect } from "react";
+import { flushSync } from "react-dom";
+import FlushSyncUI from "./FlushSyncUI";
+
+const FlushSync = () => {
+
+//   { "name": "frajdi", "surname": "malaj", "age": "21" }
+
+  const [value, setValue] = useState()
+  const [inputColor, setInputColor] = useState('black')
+  const renderCount = useRef(0)
+
+  useEffect(() => {
+   renderCount.current = renderCount.current + 1 
+  })
+  
+
+  //As we see down here when we use flush sync we can sepparate the default batching inside the 
+  //handleBlur so we set the first state that will be insinde the flushSync function and the the others that are outside.
+  const handleBlur = (event) => {
+    try {
+        JSON.parse(event.target.value)
+        setInputColor('blue')
+        setValue(event.target.value)
+        
+    } catch (error) {
+        setInputColor('red')
+        setValue(error.message)
+    }
+  } 
+
+  return (
+    <>
+      <FlushSyncUI renderCount={renderCount} inputColor={inputColor} handleBlur={handleBlur} value={value}/>
+    </>
+  );
+};
+
+export default FlushSync;
